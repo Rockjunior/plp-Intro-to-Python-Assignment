7fdacb7ea21ef3a2945bedd492f26c3133925c80
Rockjunior <r28081999m@gmail.com>
2025-03-17 13:46:38 +0300

    created list file

---
+++ lists.py
@@ -0,0 +1,20 @@
+#Empty list
+my_list = []
+
+#list with elements 10,20 30 and 40
+my_list = [10,20,30,40]
+
+#inserting elements in the list
+my_list.append(15)
+
+#extending my_list with onaoter list
+my_list.extend([50,60,70])
+
+#remove last element from the list
+my_list.pop()
+
+#sort the list in ascending order
+my_list.sort()
+
+#find and print the index of 30
+print(my_list.index(30))
